448a75c4ae99242cfad672712bfe067ce5249d0f
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-03-17 09:01:19 -0600

    remove warnings

--- lib/src/wip/autocomplete/popup_controller.dart
+++ lib/src/wip/autocomplete/popup_controller.dart
@@ -27,8 +27,9 @@ class PopupController extends ChangeNotifier {
     _selectedIndex = 0;
     isPopupShown = true;
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (itemScrollController.isAttached)
+      if (itemScrollController.isAttached) {
         itemScrollController.jumpTo(index: 0);
+      }
     });
   }
 
@@ -38,33 +39,35 @@ class PopupController extends ChangeNotifier {
 
   /// Changes the selected item and scrolls through the list of completions on keyboard arrows pressed
   void scrollByArrow(ScrollDirection direction) {
-    int previousSelectedIndex = selectedIndex;
-    if (direction == ScrollDirection.up)
+    final int previousSelectedIndex = selectedIndex;
+    if (direction == ScrollDirection.up) {
       selectedIndex =
           (selectedIndex - 1 + suggestions.length) % suggestions.length;
-    else
+    } else {
       selectedIndex = (selectedIndex + 1) % suggestions.length;
-    List<int> visiblePositions = itemPositionsListener.itemPositions.value
-        .where((item) {
-          final bool isTopVisible = item.itemLeadingEdge >= 0;
-          final bool isBottomVisible = item.itemTrailingEdge <= 1;
-          return isTopVisible && isBottomVisible;
-        })
-        .map((e) => e.index)
-        .toList();
+      final List<int> visiblePositions =
+          itemPositionsListener.itemPositions.value
+              .where((item) {
+                final bool isTopVisible = item.itemLeadingEdge >= 0;
+                final bool isBottomVisible = item.itemTrailingEdge <= 1;
+                return isTopVisible && isBottomVisible;
+              })
+              .map((e) => e.index)
+              .toList();
 
-    // List offset will be changed only if new selected item is not visible
-    if (!visiblePositions.contains(selectedIndex)) {
-      // If previously selected item was at the bottom of the visible part of the list,
-      // on 'down' arrow the new one will appear at the bottom as well
-      bool isStepDown = (selectedIndex - previousSelectedIndex == 1);
-      if (isStepDown && selectedIndex < suggestions.length - 1) {
-        itemScrollController.jumpTo(index: selectedIndex + 1, alignment: 1.0);
-      } else {
-        itemScrollController.jumpTo(index: selectedIndex);
+      // List offset will be changed only if new selected item is not visible
+      if (!visiblePositions.contains(selectedIndex)) {
+        // If previously selected item was at the bottom of the visible part of the list,
+        // on 'down' arrow the new one will appear at the bottom as well
+        final isStepDown = selectedIndex - previousSelectedIndex == 1;
+        if (isStepDown && selectedIndex < suggestions.length - 1) {
+          itemScrollController.jumpTo(index: selectedIndex + 1, alignment: 1);
+        } else {
+          itemScrollController.jumpTo(index: selectedIndex);
+        }
       }
+      notifyListeners();
     }
-    notifyListeners();
   }
 
   String getSelectedWord() => suggestions[selectedIndex];
